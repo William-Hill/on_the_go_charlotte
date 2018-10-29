3332dcd60bbb0f70f9b255e6b80cff8dfc397ad5
William Hill <mjh2225@gmail.com>
2018-10-28 22:57:40 -0700

    removed example files; added logo packs and bio image

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import github from '../img/github-icon.svg'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { fab } from '@fortawesome/free-brands-svg-icons'
+import logo from '../img/logo_pack_2/logo.png'
 
 library.add(fab)
 
@@ -12,15 +12,14 @@ const Navbar = () => (
     <div className="container">
       <div className="navbar-brand">
         <Link to="/" className="navbar-item">
-          <h1 class="title is-1">On the Go Charlotte</h1>
+          <figure className="image">
+            <img id="blk_nrd_logo" src={logo} alt="logo" style={{ width: '100px', height: '100px' }} />
+          </figure>
         </Link>
       </div>
       <div className="navbar-start">
         <Link className="navbar-item" to="/about">
           About
-        </Link>
-        <Link className="navbar-item" to="/products">
-          Products
         </Link>
       </div>
       <div className="navbar-end">
